1e1f6d073dea49134d1640adbe60703b4fdf1073
Johannes Hoppenstedt <johannes.hoppenstedt@gmail.com>
2015-01-20 21:45:10 +0100

    Introduced main function, edited License

---
+++ src/de/Boomit/ATCClient/Client.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package de.Boomit.ATCClient;
+
+/**
+ * @author Johannes Hoppenstedt
+ * (c) 2015 All rights reserved.
+ *
+ */
+public class Client {
+
+	public static Client client;
+	
+	/**
+	 * 
+	 */
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		client = new Client();
+	}
+
+}
