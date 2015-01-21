ce2cf525c0632cde3b2361cde95ebe8cba449d81
Johannes Hoppenstedt <johannes.hoppenstedt@gmail.com>
2015-01-21 17:32:13 +0100

    Configured main window and set basic paint/update parameters

--- src/de/Boomit/ATCClient/Client.java
+++ src/de/Boomit/ATCClient/Client.java
@@ -3,13 +3,23 @@
  */
 package de.Boomit.ATCClient;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
 /**
  * @author Johannes Hoppenstedt
  * (c) 2015 All rights reserved.
  *
  */
-public class Client {
+@SuppressWarnings("serial")
+public class Client extends JFrame{
 
+	//Basic variables
+	public static String version = "v. 0.1";
+	public static String name = "ATC Simulator";
+	
 	public static Client client;
 	
 	/**
@@ -17,6 +27,34 @@ public class Client {
 	 */
 	public Client() {
 		// TODO Auto-generated constructor stub
+		
+		super(name + " " + version);
+		super.setSize(900, 700);
+		super.setLocation(50, 50);
+		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		super.getContentPane().setBackground(Color.BLACK);
+		super.setResizable(false);
+		super.setVisible(true);
+		
+		
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		super.update(g);
 	}
 
 	/**
